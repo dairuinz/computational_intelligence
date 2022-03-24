7fb5ad311c664960f54c9cd9543169a129bbe78c
dairuinz <45816589+dairuinz@users.noreply.github.com>
2022-03-24 18:56:46 +0200

    Add files via upload

--- prep.py
+++ prep.py
@@ -11,7 +11,7 @@ def bow():
     df.columns = ['sentence']
     df['sentence'] = df['sentence'].str.replace('<.*?>', '', regex=True)        #deletes <int>
     # print(df.head())
-    df = df[0:100]
+    df = df[0:1000]
     # print(df)
 
     dict = {}                                       #creates dictionary from vocabs file
@@ -22,30 +22,13 @@ def bow():
 
     # print(dict.values())
 
-    # for (i, sentence) in enumerate(df['sentence']):
-    #     print(sentence)
-
-    # l = []
-    # for j in range(len(df)):
-    #     column = []
-    #     for i in range(3):
-    #         column.append(0)
-    #     l.append(column)
-    # print(l)
-
     X = []
-    for data in df.sentence:
-        v = []
-        for w in data.split():
-            k = []
-            for j in range(8520):
-                # print(j)
-                if int(w) in dict.values():
-                    # print(w)
-                    # k[j] = 0
-                    if j == w:
-                        print(j)
-                        k[j] = k[j] + 1
-            v.append(k)
-        X.append(v)
+    for y in df.sentence:
+        for w in y.split():
+            x_counter = [0] * 8519
+            for i in range(8520):
+                if int(w) == i:
+                    x_counter[i] = x_counter[i] + 1
+        X.append(x_counter)
     print(X)
+    print(len(X), 'x', len(X[0]), sep='')
